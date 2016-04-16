56e2edcbc97e6c5f9b27fbdac6dbae127f419e47
Chris Gaona <thebeloved88@sbcglobal.net>
2016-04-15 20:47:18 -0700

    Adds delete functionality to delete note...with controller functionality

--- app/assets/javascript/controllers/NotesShowController.js
+++ app/assets/javascript/controllers/NotesShowController.js
@@ -3,11 +3,16 @@
 
   angular.module('NoteWrangler')
 
-  .controller('NotesShowController', function(Note, $routeParams) {
+  .controller('NotesShowController', function(Note, $routeParams, $location) {
     var vm = this;
 
     vm.note = Note.get({id: $routeParams.id});
 
+    vm.deleteNote = function(note) {
+      note.$remove().then(function() {
+        $location.path('/notes');
+      });
+    };
   });
 
 })();
